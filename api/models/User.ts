103f16c7ed4f37b11134a94ad01d5282b883d610
mert <mert.izgahi@outlook.com>
2024-03-14 15:51:48 +0300

    create-user-model

---
+++ api/models/User.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+export interface IUser {
+    _id?: mongoose.Schema.Types.ObjectId;
+    name: string;
+    email?: string;
+    password: string;
+    bio: string;
+    socials?: {
+        [key: string]: string;
+    };
+    createdAt?: Date;
+    updatedAt?: Date;
+}
+
+const userSchema = new mongoose.Schema<IUser>(
+    {
+        name: { type: String, required: [true, "Name is required"] },
+        email: {
+            type: String,
+            required: [true, "Email is required"],
+            unique: [true, "Email already exists"],
+        },
+        password: { type: String, required: [true, "Password is required"] },
+        bio: { type: String },
+        socials: {
+            type: Map,
+            of: String,
+            default: {},
+        },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+const User = mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
+export default User;
